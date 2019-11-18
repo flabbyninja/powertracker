3383cb4cc1706fbcbf452e5fd9fbb1fd3348cec0
Flabby Ninja <dev@flabbyninja.net>
2019-11-18 15:18:24 +0000

    Initial version

---
+++ src/main/java/com/flabbyninja/powertracker/PowertrackerApplication.java
@@ -0,0 +1,13 @@
+package com.flabbyninja.powertracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowertrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowertrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flabbyninja/powertracker/PowertrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flabbyninja.powertracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowertrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
