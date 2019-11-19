ff32a724b367fce4d81df8ea3ed161d1e879eff1
Flabby Ninja <dev@flabbyninja.net>
2019-11-19 11:23:57 +0000

    Test for encrypted and clear properties

--- src/test/java/com/flabbyninja/powertracker/PowertrackerApplicationTests.java
+++ src/test/java/com/flabbyninja/powertracker/PowertrackerApplicationTests.java
@@ -1,13 +1,31 @@
 package com.flabbyninja.powertracker;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @SpringBootTest
 class PowertrackerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private PropertyService propService;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void validateClearProperty() {
+        assertThat(propService.getProperty()).isEqualTo("test_clear_value");
+    }
+
+    @Test
+    void validateEncryptedProperty() {
+		System.out.println(propService.getEncProperty());
+		assertThat(propService.getEncProperty()).isEqualTo("test_encrypted_value");
+    }
 
 }
