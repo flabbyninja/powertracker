54511d70689311d643ca4fdb1ff19f8a904140c2
Flabby Ninja <dev@flabbyninja.net>
2019-11-26 19:06:13 +0000

    Remove unnecessary stock method

--- powertracker-rest/src/main/java/com/flabbyninja/powertracker/jparepositories/PowerItemRepository.java
+++ powertracker-rest/src/main/java/com/flabbyninja/powertracker/jparepositories/PowerItemRepository.java
@@ -11,9 +11,6 @@ public interface PowerItemRepository extends CrudRepository<PowerItem, Long> {
 
     List<PowerItem> findByBrand(String brand);
 
-    @Query(value = "SELECT count(*) FROM power_item", nativeQuery = true)
-    int countEntities();
-
     @Query(value = "select MIN(id) from power_item where power_size=:power_size and available=:available", nativeQuery = true)
     long getByPowerSizeAndAvailability(@Param("power_size") String powerSize, boolean available);
 
