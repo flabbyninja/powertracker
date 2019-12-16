a3430c839e9aee48ee379bbb1afc1b4d1e2498f3
Flabby Ninja <dev@flabbyninja.net>
2019-12-16 11:08:46 +0000

    Optional cleanup

--- powertracker-rest/src/test/java/com/flabbyninja/powertracker/integration/PowerTrackerRepoTests.java
+++ powertracker-rest/src/test/java/com/flabbyninja/powertracker/integration/PowerTrackerRepoTests.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Optional;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -27,23 +25,8 @@ class PowerTrackerRepoTests {
     void retrieveSingleExisting() {
         PowerItem sourceItem = new PowerItem("Worzel", "SocialPower", "PP9", "NiCad", 4000L, false, "Stockroom");
         Long savedItemId = powerRepo.save(sourceItem).getId();
-        PowerItem getExisting = powerRepo.findById(savedItemId).get();
+        PowerItem getExisting = powerRepo.findById(savedItemId).orElse(null);
         assertThat(sourceItem).isEqualTo(getExisting);
-    }
-
-    @Test
-    void retrieveSingleMissing() {
-        assertThat(powerRepo.findById(9999L)).isEqualTo(Optional.empty());
-    }
-
-    @Test
-    void findAll() {
-        powerRepo.findAll().forEach(System.out::println);
-    }
-
-    @Test
-    void findByBrand() {
-        powerRepo.findByBrand("Worzel").forEach(System.out::println);
     }
 
     @Test
